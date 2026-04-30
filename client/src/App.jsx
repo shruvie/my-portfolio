f784aaa8d9247365b855b752aeeff10c0c9dc79c
Shruti Verma <shrutiverma10@gmail.com>
2026-04-30 18:53:59 +0530

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-import Navbar from './components/Navbar/Navbar.js'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
       <div>
         <Navbar />
-        <h1>Welcome im shrutiiiii</h1>
       </div>
   )
 }
