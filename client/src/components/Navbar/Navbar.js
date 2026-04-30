717b28a95a87d943603ed57577c54ed004e958dc
Shruti Verma <shrutiverma10@gmail.com>
2026-04-30 18:42:10 +0530

    navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navbar() {
+    return(
+        <nav>
+            <h4>Shruti's Portfolio</h4>
+            <ol>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Education</li>
+                <li>Contact Us</li>
+            </ol>
+            <button>Resume</button>
+        </nav>
+    )
+};
